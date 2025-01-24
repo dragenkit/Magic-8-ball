dab935ac32d2576ebe62418cb032b6e719ec7f21
dragenkit <marvinauner47@gmail.com>
2025-01-24 22:27:08 +0100

    add random number from 1 to 8

--- src/MagicEigthBall.java
+++ src/MagicEigthBall.java
@@ -1,7 +1,8 @@
 class MagicEigthBall {
 
     public static MagicEigthBall(){
-
+        int eigthBallNumber = (int)Math.random() * 9;
+        System.out.println(eigthBallNumber);
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 class Main {
 
     public static void main(String[] args) {
+        MagicEigthBall magicBall = new MagicEigthBall;
 
     }
 }
